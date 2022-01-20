82d0390af38e0d3e622fa0c6d40c01fa06779995
Wilson Mongwe <wilsonmongwe@Wilsons-Air.lan>
2022-01-20 10:15:20 +0200

    explore_target methods added

--- utilities/explore_target.py
+++ utilities/explore_target.py
@@ -17,6 +17,33 @@ class ExploreTarget(object):
               print( name+ "  -----> ",  chain)
               self.results[name + "_" + str(chain)] = self.samplers[s].run()
               
-             
+  def ess(self):
+      return
+
+  def r_hat(self):
+      return
+  
+  def mean_likelihood(self, x_test = 0, y_test = 0):
+      return
+  
+  def plot_ard_importances(self):
+      # ensure that the samplers return the samples and alphas seperately
+      return
+  
+  def predictive_performnce(self, x_test, y_test):
+      # mse, auc etc
+      return
+
+  def plot_log_prob(self):
+      # likelihoods for different samplers
+      return
+  
+  def plot_ess(self):
+      # ess over time, grad val, func val
+      return
+  
+  def plot_r_hat(self):
+      return
+          
           
 
