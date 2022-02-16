1c7aefa979e630f00a7e458adbe78d9f00325c18
Wilson Mongwe <wilsonmongwe@Wilsons-Air.lan>
2022-02-16 08:13:43 +0200

    added s2hmc and tests

---
+++ samplers/s2hmc.py
@@ -0,0 +1,228 @@
+from samplers.hmc import HamiltonianMonteCarlo
+from torch.autograd import grad
+from utilities.utils import leapfrog_intergrator as leap
+from utilities.utils import metropolis_acceptance_step as mh_step
+from utilities.utils import adaptation
+from utilities.utils import adaptation_params
+from tqdm import tqdm
+
+import time
+import numpy as np
+import torch
+
+class SeperableShadowHybridMonteCarlo(HamiltonianMonteCarlo):
+  def __init__(self, model,  weights_0, sample_size, burn_in_period,
+               adapt, target_acceptance, step_size, path_length):
+      HamiltonianMonteCarlo.__init__(self, model,  weights_0, sample_size, burn_in_period,
+                   adapt, target_acceptance, step_size, path_length)
+      
+      self.tolerance = 1e-6
+      self.max_iterations = 100
+      
+  def target(self, weights):
+      self.no_target_evaluations += 1
+      return self.model.log_prob(weights)
+  
+  def kinetic(self, momentum):
+      return 0.5 * torch.matmul(momentum, momentum.T)
+  
+  def hamiltonian(self, weights, momentum):
+      return self.kinetic(momentum) + self.target(weights)
+    
+  def shadow_hamiltonian(self, weights, p):
+      # Compute shadow: S(x,p) = H(x,p) + dt2 / 24 UT_x M-1 U_x
+      loss = self.target(weights)
+      force = self.compute_gradients(loss,weights)
+      dt2d24 = self.step_size * self.step_size / 24.0
+      H_x_p = self.hamiltonian(weights, p) 
+      Uq_M_Uq = torch.matmul(force.T,force)
+      shadow  = H_x_p + dt2d24 * Uq_M_Uq
+      return shadow
+  
+  def shadow_target(self, weights):
+      loss = self.target(weights)
+      force = self.compute_gradients(loss,weights)
+      dt2d24 = self.step_size * self.step_size/24.0
+      Uq_M_Uq = torch.matmul(force.T,force)
+
+      return self.target(weights) + dt2d24 * Uq_M_Uq
+  
+  def solve_for_p_hat(self, dt, dt_24, weights, momentum):
+      new_p_hat = momentum
+      norm = 1
+      count = 0
+      while (norm > self.tolerance):
+          prev_p_hat = new_p_hat
+          
+          forward = weights + dt * prev_p_hat
+          backward = weights - dt * prev_p_hat
+          loss_forward = self.target(forward)
+          loss_backward = self.target(backward)
+            
+          U_x_forward = self.compute_gradients(loss_forward, forward)
+          U_x_back = self.compute_gradients(loss_backward, backward)
+          
+          new_p_hat = momentum - dt_24 * (U_x_forward - U_x_back)
+          norm = torch.norm(new_p_hat - prev_p_hat)
+          count +=1
+          if (count > self.max_iterations):
+              break
+      return new_p_hat
+    
+  def pre_processing(self, momentum, weights):
+        dt = self.step_size
+        dt_24 = dt / 24.0
+        dt2_24 = dt * dt_24
+        
+        new_p_hat = self.solve_for_p_hat(dt, dt_24, weights, momentum)
+        
+        forward = weights + dt * new_p_hat
+        backward = weights - dt * new_p_hat
+        loss_forward = self.target(forward)
+        loss_backward = self.target(backward)
+            
+        U_x_forward = self.compute_gradients(loss_forward, forward)
+        U_x_back = self.compute_gradients(loss_backward, backward)
+        
+        weights_hat = weights + dt2_24 * (U_x_forward + U_x_back) 
+
+        return new_p_hat, weights_hat
+    
+  def solve_for_weights_hat(self, weights_hat, momentum_hat, dt, dt2_24, U_x):
+        new_weights = weights_hat
+        norm = 1
+        count = 0
+        while (norm > self.tolerance):
+            prev_weights = new_weights
+            
+            forward = prev_weights + dt * momentum_hat
+            backward = prev_weights - dt * momentum_hat
+            loss_forward = self.target(forward)
+            loss_backward = self.target(backward)
+            
+            U_x_back = self.compute_gradients(loss_forward, forward)
+            U_x_forward = self.compute_gradients(loss_backward, backward)
+        
+            new_weights = weights_hat -  dt2_24 * (U_x_back + U_x_forward) 
+          
+            norm = torch.norm(new_weights - prev_weights,2)
+            count += 1
+            if (count > self.max_iterations):
+                break
+        return new_weights
+
+  def post_processing(self, momentum_hat, weights_hat):
+        dt = self.step_size
+        loss = self.target(weights_hat)
+        U_x = self.compute_gradients(loss, weights_hat)
+        U_x = U_x.T
+ 
+        dt_24 = dt / 24.0
+        dt2_24 = dt * dt_24
+       
+        new_weights = self.solve_for_weights_hat(weights_hat, momentum_hat, dt, dt2_24, U_x)
+        
+        forward = new_weights + dt * momentum_hat
+        backward = new_weights - dt * momentum_hat
+        loss_forward = self.target(forward)
+        loss_backward = self.target(backward)
+            
+        U_x_back = self.compute_gradients(loss_forward, forward)
+        U_x_forward = self.compute_gradients(loss_backward, backward)
+          
+        momentum = momentum_hat + dt_24 * (U_x_forward - U_x_back)
+
+        return momentum, new_weights    
+     
+  def compute_gradients(self,log_p_eval_at_w, w):
+      self.no_grad_evaluations += 1
+      return grad(log_p_eval_at_w, w)[0]
+    
+  def transition(self, weights, momentum):
+      weights, momentum = leap(weights, momentum, self.step_size, 
+                               self.path_length, self.shadow_target, 
+                               self.compute_gradients)
+      return weights, momentum
+
+  def run(self):
+      print(":::::::::: Running S2HMC :::::::::::")
+  
+      # initialise variables      
+      samples = []
+      log_lik = []
+      weights = self.weights_0
+      n_accepted = 0
+      importance_weights = [] #weighting for observables
+      
+      # initialise adaptation parameters
+      Hbar_old, eps_bar, mu = adaptation_params(self.step_size)
+
+      for i in tqdm(range(self.sample_size)): 
+          
+          momentum = torch.normal(0,1,(self.dim,))
+          
+          old_shadow_hamiltonian = self.shadow_hamiltonian(weights, momentum)
+          
+          momentum_hat, weights_hat = self.pre_processing(momentum, weights)
+          
+          new_weights, new_momentum = self.transition(weights_hat, momentum_hat)
+          
+          new_momentum , new_weights = self.post_processing(new_momentum, new_weights)
+          
+          new_shadow_hamiltonian = self.shadow_hamiltonian(new_weights, new_momentum)
+             
+          new_Hamiltonian = self.hamiltonian(new_weights, new_momentum)
+          
+          # store importance weights
+          importance_weights.append((torch.exp(new_shadow_hamiltonian-new_Hamiltonian)).detach().clone().numpy())
+             
+          u = torch.rand(())
+          accept_reject, alpha = mh_step(u,new_shadow_hamiltonian, old_shadow_hamiltonian, i) 
+          
+    
+          if accept_reject :
+             weights = new_weights.detach().clone()
+             n_accepted +=1
+             samples.append(weights.detach().clone().numpy())
+             log_lik.append(self.target(weights).detach().clone().numpy())
+    
+          else:
+              samples.append(weights.detach().clone().numpy())
+              log_lik.append(self.target(weights).detach().clone().numpy())
+      
+          if self.adapt:
+              if i < self.burn_in_period:
+                 self.step_size, eps_bar, Hbar_old = adaptation(i, mu, Hbar_old, alpha.detach().numpy(), 
+                                                                   eps_bar, self.target_acceptance)
+              else:
+                 self.step_size = eps_bar 
+    
+                     
+          if i == self.burn_in_period:
+              start_time = time.time()
+    
+    
+      end_time = time.time()
+      total_time = (end_time - start_time)
+  
+      accepted_rate = (n_accepted / self.sample_size)*100
+      print("\n")
+      print("Total time :", total_time)  
+      print("Acceptance Rate: ", accepted_rate, "%")
+      print('Final Step Size: ', self.step_size )     
+      
+      results = {
+                  "samples": np.asarray(samples),
+                  "log_like": np.asarray(log_lik),
+                  "importance_weights": np.asarray(importance_weights),
+                  "step_size": self.step_size,
+                  "total_time": total_time,
+                  "accepted_rate": accepted_rate,
+                  "no_grad_evaluations": self.no_grad_evaluations,
+                  "no_target_evaluations": self.no_target_evaluations,
+                }
+      self.no_grad_evaluations = 0
+      self.no_target_evaluations = 0
+      print(":::::::::: S2HMC Finished:::::::::::")    
+      return results
+    
---
+++ tests/s2hmc_tests.py
@@ -0,0 +1,92 @@
+import unittest
+from samplers.s2hmc import SeperableShadowHybridMonteCarlo as s2hmc
+import torch
+import numpy as np
+from models.blr import LogisticRegression as BLR
+from utilities.utils import magnetic_field_exp_and_factor
+
+
+# test setup
+X = torch.tensor([[0.1, 0.1], [0.2, 0.2]])
+Y = torch.tensor([ 1, 0])
+dim = 3
+ard = False
+model = BLR(X, Y, dim, ard)
+
+sample_size = 5
+burn_in_period = 2
+adapt = False
+target_acceptance = 0.8 
+step_size = 1e-1
+weights = torch.tensor([0.1, 0.2, 0.3])
+momentum = torch.tensor([0.11, 0.12, 0.31])
+path_lehth = 1
+
+G = torch.tensor([[0.0, 0.1, 0.1], [0.1, 0.2, 0.0] , [0.0, 0.1, 0.1]])
+G = (G -G.T)/ 2
+
+sampler = s2hmc(model, weights, sample_size, burn_in_period, adapt, 
+               target_acceptance, step_size, path_lehth)
+sampler_1 = s2hmc(model, weights, sample_size, burn_in_period, adapt, 
+                 target_acceptance, step_size, path_lehth)
+
+sampler_2 = s2hmc(model, weights, sample_size, burn_in_period, adapt, 
+                 target_acceptance, step_size, 3)
+
+class TestHMCMethods(unittest.TestCase):
+    
+    def setUp(self):
+        print ("In method", self._testMethodName)
+    
+    def test_s2hmc_initialisation(self):
+        self.assertEqual(sampler.model.ard, ard)
+        self.assertEqual(sampler.model.dimensions, dim)
+        self.assertEqual(sampler.adapt, adapt)
+        self.assertEqual(sampler.sample_size, sample_size)
+        self.assertEqual(sampler.burn_in_period, burn_in_period)
+        self.assertEqual(sampler.target_acceptance, target_acceptance)
+        self.assertEqual(sampler.step_size, step_size)
+        self.assertEqual(sampler.path_length, path_lehth)
+        self.assertTrue(np.array_equal(sampler.weights_0.detach().numpy(), 
+                                       weights.detach().numpy(), equal_nan=True))
+      
+    def test_s2hmc_target(self):
+        sampler_1.target(weights)
+        sampler_1.target(weights)
+        self.assertEqual(sampler_1.no_target_evaluations, 2)
+        
+   
+    def test_s2hmc_transition(self):
+        return
+        
+        
+    def test_s2hmc_run(self):
+        torch.manual_seed(10)
+        result = sampler_2.run()
+
+        self.assertEqual(result["no_grad_evaluations"], 123)
+        self.assertEqual(result["no_target_evaluations"], 143)
+        self.assertEqual(result["accepted_rate"], 100.0)
+        
+        expected_samples = np.array([[-0.08539007, -0.11126358,  0.1908932 ],
+         [-0.4466915,  -0.34187567, -0.11110405],
+         [-0.6739865 , -0.43809247,  0.2552854 ],
+         [-0.84375435, -0.6131196 ,  0.3798303 ],
+         [-0.8044727,  -0.2639995 ,  0.6386736 ]])
+        
+        expected_log_like = np.array([4.167862,  4.2815695, 4.44591,   4.6941357, 4.709428 ]) 
+        
+        expected_weights = np.array([1.000032,  1.000133,  1.000258,  1.0004717, 1.0005475]) 
+
+        
+        self.assertTrue(np.allclose(result["samples"], 
+                                    expected_samples, rtol = 1e-7, equal_nan=True,))
+        self.assertTrue(np.allclose(result["log_like"], 
+                                    expected_log_like, rtol = 1e-7, equal_nan=True,)) 
+        self.assertTrue(np.allclose(result["importance_weights"], 
+                                    expected_weights, rtol = 1e-7, equal_nan=True,))
+        
+  
+if __name__ == '__main__':
+    unittest.main()
+   
